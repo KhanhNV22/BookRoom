699cbee4ae302d22fafb5072622ef5c4eb61a55d
KhanhNV22 <khanhnv2@rikkeisoft.com>
2022-04-19 08:39:43 +0700

    up

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:4000';
+export const API_URL = 'https://frozen-escarpment-10165.herokuapp.com';
 export const GOOGLE_CLIENT_ID = '422653143846-21pcn0fknnquh0hs9881tbkhnn4f855d.apps.googleusercontent.com';
 export const user = localStorage.getItem('userName');
