84791dbad6641d6b4efaaeabbe014c3022f0044d
KhanhNV22 <khanhnv2@rikkeisoft.com>
2022-04-22 15:06:16 +0700

    update

--- src/pages/header/HeaderMobile.tsx
+++ src/pages/header/HeaderMobile.tsx
@@ -1,25 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { userIdName } from "../../services/userService";
 
 interface IMenuProps {
   isMenuOpen: boolean;
 }
 
 const HeaderMobile = ({ isMenuOpen }: IMenuProps) => {
+  let navigate = useNavigate();
+
+  const userNameGG = localStorage.getItem('userNameGG')
+
+  function logout() {
+    localStorage.clear();
+    navigate("/")
+    window.location.reload();
+  }
   return (
     <div className={`app-menu ${isMenuOpen ? "menu-open" : ""}`}>
-      <h2>App Menu Mobile</h2>
-      <ul>
-          <li>
+      {userIdName ?
+        <div>
+          <h2>{userIdName}</h2>
+          <ul>
+            <li>
               <Link to="/host">Host</Link>
-          </li>
-          <li>
+            </li>
+            <li>
+              <Link to="/settingUser">Danh Sách Đặt Phòng</Link>
+            </li>
+            <li>
+              <span className="menu_link-span" onClick={logout}>Đăng xuất</span>
+            </li>
+          </ul>
+        </div> :
+        <div>
+          <h2>App Menu Mobile</h2>
+          <ul>
+            <li>
+              <Link to="/host">Host</Link>
+            </li>
+            <li>
               <Link to="/login">Đăng Nhập</Link>
-          </li>
-          <li>
+            </li>
+            <li>
               <Link to="/registration">Đăng Ký</Link>
-          </li>
-      </ul>
+            </li>
+          </ul>
+        </div>
+      }
+
     </div>
   );
 };
